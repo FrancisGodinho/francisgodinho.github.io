9b9944a0d2bd08009b76c0d51b4b5d5031e8074c
Francis Godinho <francisgodinho2010@hotmail.com>
2023-07-19 21:27:35 -0700

    added submodule and fixed install

--- leetcode-v2/src/App.tsx
+++ leetcode-v2/src/App.tsx
@@ -5,11 +5,11 @@ import Editor from "react-run-code";
 
 function App() {
   return (
-    <div>
+    <div style={{backgroundColor: "#1e1e1e", height: "100vh"}}>
       <Editor id="10" modelsInfo={[{
         filename: "test.py",
         value: "def hello_world(): \n",
-        language: "javascript",
+        language: "python",
       }]} />
     </div>
   );
